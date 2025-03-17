132be6f7c7ad7394ae276fb4ea50340faec91ca3
mariaDLCj <152199801+mariaDLCj@users.noreply.github.com>
2025-03-17 09:24:33 +0100

    Delete src/main directory

--- src/main/webapp/INC/pie.jsp
+++
@@ -1,4 +0,0 @@
-
-<footer class="text-center d-flex justify-content-center align-items-center">
-        <p class="highlight pt-3">Corporación Enigma</p>
-</footer>
--- src/main/webapp/JS/scriptAudioAmbiente.js
+++
@@ -1,9 +0,0 @@
-// Función para reproducir el audio
-function reproducirAudio() {
-    const audio = document.getElementById("audio");
-    audio.muted = false; // Asegura que el audio no esté en silencio
-    audio.play(); // Reproduce el audio
-}
-
-// Ejecutar la función cuando la página haya cargado completamente
-window.onload = reproducirAudio();
